93fb6e5f8ef906c342acc38342af9bc8a91c8ff1
Rochakdh <39098485+Rochakdh@users.noreply.github.com>
2020-05-23 14:26:45 +0545

    Update 002. Counting Valleys.py
    I tried for the string UDDDUD which shouldn't be the valley even though we encounter  -1  in while moving down.So I think we should look for U and level 0.

--- Interview Preparation Kit - Python/01. Warm-up Challenges/002. Counting Valleys.py
+++ Interview Preparation Kit - Python/01. Warm-up Challenges/002. Counting Valleys.py
@@ -8,10 +8,12 @@ def counting_valleys(n, s):
     for i in range(n):
         if s[i] == 'U':
             current_level += 1
+            if current_level == 0:
+                count += 1
         elif s[i] == 'D':
             current_level -= 1
-            if current_level == -1:
-                count += 1
+#             if current_level == -1:
+#                 count += 1
     return count
 
 
